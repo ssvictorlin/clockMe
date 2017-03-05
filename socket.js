666126d45b613b0669ded19a704e86bf484725c8
ssvictorlin <ssvictorlin@gmail.com>
2017-03-04 20:18:36 -0800

    add simple hand swipe dectection

--- socket.js
+++ socket.js
@@ -1,8 +1,11 @@
 var usonic = require('mmm-usonic');
+var leftState = false;
+var rightState = false;
+
 
 module.exports = function (io) {
 
-    //setInterval(measureDistance, 100);
+    setInterval(measureDistance, 100);
     io.sockets.on('connection', function (socket) {
         console.log("sending message..."); 
         socket.emit('welcome', { message: 'Welcome!'});
@@ -17,5 +20,30 @@ function measureDistance() {
     console.log("Left distance is " + distanceLeft);
     var distanceRight = sensorRight();
     console.log("Right distance is " + distanceRight);
-}
 
+    if (!leftState && !rightState) {
+        if (distanceLeft < 20) {
+            leftState = true;
+        }
+        else if (distanceRight < 20) {
+            rightState = true;
+        }
+    } 
+    else if (leftState && rightState) {
+        console.log("both too close...");
+        leftState = false;
+        rightState = false;
+    }
+    else {
+        if (leftState && distanceRight < 20) {
+
+            console.log("Swipe right...");
+            leftState = false;
+        }
+        else if (rightState && distanceLeft < 20) {
+
+            console.log("Swipe left...");
+            rightState = false;
+        }
+    }
+}
